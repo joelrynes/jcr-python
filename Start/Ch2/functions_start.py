a66326c94835da3144e010dec7a772762073f142
joelrynes <joelrynes@yahoo.com>
2025-11-10 18:56:59 -0500

    Update functions_start.py

--- Start/Ch2/functions_start.py
+++ Start/Ch2/functions_start.py
@@ -1,19 +1,45 @@
 # LinkedIn Learning Python course by Joe Marini
 # Example file for working with functions
 
-
 # define a basic function
-print("hello world!")
-name = input("What is your name? ")
-print("Nice to meet you,", name)
+def hello_func():
+  print("hello world!")
+  name = input("What is your name? ")
+  print("Nice to meet you,", name)
+
+hello_func()
 
 # function that takes parameters
+def hello_func(greeting):
+  print("hello world!")
+  name = input("What is your name? ")
+  print(greeting, name)
+  print()
 
+hello_func("How are you doing")
+hello_func("Hey what's up")
 
 # function that returns a value
+def cube(x):
+  return x*x*x
 
+result = cube(4)
+print(result)
 
 # function with default value for an parameter
+def hello_func(greeting, name=None):
+  print("hello world!")
+  if name == None:
+    name = input("What is your name? ")
+  print(greeting, name)
 
+hello_func("Nice to meet you", None)
 
 # function with variable number of parameters
+def multi_add(start, *args):
+  result = start
+  for x in args:
+    result = result + x
+  return result
+
+print(multi_add(10,4,5,10,4,10))
